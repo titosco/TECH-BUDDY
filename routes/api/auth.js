ffb01583d9f9904bf8b7f5df978903a6bceea05e
titosco <titobiloluwatella@gmail.com>
2023-10-20 00:38:00 +0100

    auth

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/User')
 
@@ -16,5 +20,60 @@ router.get('/', auth, async(req, res) => {
 		res.status(500).send('Server Error');
 	}
 });
+
+// @route	POST api/auth
+// @desc	Authenticate user & get token
+// @access	public
+router.post(
+	'/',
+	[
+		check('email', 'Please include a valid email').isEmail(),
+		check(
+			'password',
+			'Password is required'
+		).exists()
+	], 
+	async(req, res) => {
+	const errors = validationResult(req);
+	if(!errors.isEmpty()) {
+		return res.status(400).json({ errors: errors.array() })
+	}
+
+	const { email, password} = req.body;
+
+	try {
+		let user = await User.findOne({ email});
+
+		if(user) {
+			return res.status(400).json({ errors: [{ mssg: 'Ivalid user'}]});
+		}
+
+		
+
+		const payload = {
+			user: {
+				id: user.id
+			}
+		}
+
+		jwt.sign(
+			payload,
+			config.get('jwtSecret'),
+			{ expiresIn: 360000},
+			(err, token) => {
+				if(err) throw err;
+				res.json({ token });
+			});
+
+	} catch(err) {
+		console.error(err.message);
+		res.statusCode(500).send('server error');
+	}
+
+	
+
+
+});
+
 
 module.exports = router;
